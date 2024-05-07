4d69e7300c7b62d2335415c18f1669b80e64aebf
sakorn  moontha <kornfbs@outlook.com>
2024-05-08 05:49:18 +0700

    google login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { createClient } from '@/utils/supabase/client'
+import { Provider } from '@supabase/supabase-js'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import Image from "next/image";
 
-export default function LoginPage() {
+export default function LoginPage(this: any) {
 	const supabase = createClient()
 	const router = useRouter()
 
@@ -27,6 +29,16 @@ export default function LoginPage() {
 		router.push('/')
 	}
 
+	async function google(provider: Provider) {
+		await supabase.auth.signInWithOAuth({
+			provider,
+			options: {
+				redirectTo: `${location.origin}/auth/callback`,
+			}
+		})
+	
+	}
+
 	return (
 		<div className="bg-gray-100 h-screen flex items-center justify-center">
 			<div className="bg-white p-8 rounded shadow-lg w-80">
@@ -34,14 +46,14 @@ export default function LoginPage() {
 				<form>
 					<div className="mb-4">
 						<label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-						<input type="email"  onChange={(e) => setEmail(e.target.value)} id="email" name="email" className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+						<input type="email"  onChange={(e) => setEmail(e.target.value)} id="email" name="email" className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"  />
 					</div>
 					<div className="mb-4">
 						<label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-						<input onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+						<input onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" className="w-full border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"  />
 					</div>
 					<button formAction={login} type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 mb-3">Login</button>
-					<button formAction={signup} type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Signup</button>
+					<button formAction={google.bind(this, 'google')} type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 flex justify-center items-center gap-2"><Image className='bg-white p-1 rounded-full' src='/icons/google-icon.svg' alt="google" width={20} height={20}/>Google Login</button>
 				</form>
 			</div>
 		</div>
